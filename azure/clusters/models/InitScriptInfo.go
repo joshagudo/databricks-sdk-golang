83da77fa2e0640000802b5038f7c879d05fadd14
suganya srinivasan <suganyasrinivasan@suganyas-MacBook-Pro.local>
2021-05-18 10:54:30 +1000

    add changes from review- Jacob

--- azure/clusters/models/InitScriptInfo.go
+++ azure/clusters/models/InitScriptInfo.go
@@ -2,5 +2,5 @@ package models
 
 type InitScriptInfo struct {
 	Dbfs *DbfsStorageInfo `json:"dbfs,omitempty" url:"dbfs,omitempty"`
-	fs   *FileStorageInfo `json:"fs,omitempty" url:"fs,omitempty"`
+	Fs   *FileStorageInfo `json:"fs,omitempty" url:"fs,omitempty"`
 }
