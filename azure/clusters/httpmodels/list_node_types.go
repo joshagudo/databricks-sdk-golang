ff9b408a296747067474f9211a356557564f26a1
suganya srinivasan <suganyasrinivasan@suganyas-MacBook-Pro.local>
2021-05-18 10:57:50 +1000

    add changes from review- Jacob

--- azure/clusters/httpmodels/list_node_types.go
+++ azure/clusters/httpmodels/list_node_types.go
@@ -5,11 +5,15 @@ import (
 )
 
 type ListNodeTypesRespItem struct {
-	NodeTypeID     string                        `json:"node_type_id,omitempty" url:"node_type_id,omitempty"`
-	MemoryMb       int32                         `json:"memory_mb,omitempty" url:"memory_mb,omitempty"`
-	NumCores       float32                       `json:"num_cores,omitempty" url:"num_cores,omitempty"`
-	Description    string                        `json:"description,omitempty" url:"description,omitempty"`
-	InstanceTypeID string                        `json:"instance_type_id,omitempty" url:"instance_type_id,omitempty"`
-	IsDeprecated   bool                          `json:"is_deprecated,omitempty" url:"is_deprecated,omitempty"`
+	NodeTypeID     string                               `json:"node_type_id,omitempty" url:"node_type_id,omitempty"`
+	MemoryMb       int32                                `json:"memory_mb,omitempty" url:"memory_mb,omitempty"`
+	NumCores       float32                              `json:"num_cores,omitempty" url:"num_cores,omitempty"`
+	Description    string                               `json:"description,omitempty" url:"description,omitempty"`
+	InstanceTypeID string                               `json:"instance_type_id,omitempty" url:"instance_type_id,omitempty"`
+	IsDeprecated   bool                                 `json:"is_deprecated,omitempty" url:"is_deprecated,omitempty"`
 	NodeInfo       *models.ClusterCloudProviderNodeInfo `json:"node_info,omitempty" url:"node_info,omitempty"`
+}
+
+type ListNodeTypesRespItemList struct {
+	NodeTypes []ListNodeTypesRespItem `json:"node_types,omitempty" url:"node_types,omitempty"`
 }
