a2affb0aafb3693f51b38459c301bdf701a9d984
Siliang Jiao <Siliang.Jiao@microsoft.com>
2021-05-18 10:28:24 +0800

    modify para naming for dbfs and groups

--- azure/dbfs.go
+++ azure/dbfs.go
@@ -18,89 +18,89 @@ func (a DbfsAPI) init(client DBClient) DbfsAPI {
 }
 
 // AddBlock appends a block of data to the stream specified by the input handle
-func (a DbfsAPI) AddBlock(data httpmodels.AddBlockReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/add-block", data, nil)
+func (a DbfsAPI) AddBlock(req httpmodels.AddBlockReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/add-block", req, nil)
 	return err
 }
 
 // Close closes the stream specified by the input handle
-func (a DbfsAPI) Close(data httpmodels.CloseReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/close", data, nil)
+func (a DbfsAPI) Close(req httpmodels.CloseReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/close", req, nil)
 	return err
 }
 
 // Create opens a stream to write to a file and returns a handle to this stream
-func (a DbfsAPI) Create(data httpmodels.CreateReq) (httpmodels.CreateResp, error) {
-	var createResponse httpmodels.CreateResp
+func (a DbfsAPI) Create(req httpmodels.CreateReq) (httpmodels.CreateResp, error) {
+	var resp httpmodels.CreateResp
 
-	resp, err := a.Client.performQuery(http.MethodPost, "/dbfs/create", data, nil)
+	jsonResp, err := a.Client.performQuery(http.MethodPost, "/dbfs/create", req, nil)
 	if err != nil {
-		return createResponse, err
+		return resp, err
 	}
 
-	err = json.Unmarshal(resp, &createResponse)
-	return createResponse, err
+	err = json.Unmarshal(jsonResp, &resp)
+	return resp, err
 }
 
 // Delete deletes the file or directory (optionally recursively delete all files in the directory)
-func (a DbfsAPI) Delete(data httpmodels.DeleteReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/delete", data, nil)
+func (a DbfsAPI) Delete(req httpmodels.DeleteReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/delete", req, nil)
 	return err
 }
 
 // GetStatus gets the file information of a file or directory
-func (a DbfsAPI) GetStatus(data httpmodels.GetStatusReq) (httpmodels.GetStatusResp, error) {
-	var fileInfo httpmodels.GetStatusResp
+func (a DbfsAPI) GetStatus(req httpmodels.GetStatusReq) (httpmodels.GetStatusResp, error) {
+	var resp httpmodels.GetStatusResp
 
-	resp, err := a.Client.performQuery(http.MethodGet, "/dbfs/get-status", data, nil)
+	jsonResp, err := a.Client.performQuery(http.MethodGet, "/dbfs/get-status", req, nil)
 	if err != nil {
-		return fileInfo, err
+		return resp, err
 	}
 
-	err = json.Unmarshal(resp, &fileInfo)
-	return fileInfo, err
+	err = json.Unmarshal(jsonResp, &resp)
+	return resp, err
 }
 
 // List lists the contents of a directory, or details of the file
-func (a DbfsAPI) List(data httpmodels.ListReq) (httpmodels.ListResp, error) {
-	var listResponse httpmodels.ListResp
+func (a DbfsAPI) List(req httpmodels.ListReq) (httpmodels.ListResp, error) {
+	var resp httpmodels.ListResp
 
-	resp, err := a.Client.performQuery(http.MethodGet, "/dbfs/list", data, nil)
+	jsonResp, err := a.Client.performQuery(http.MethodGet, "/dbfs/list", req, nil)
 	if err != nil {
-		return listResponse, err
+		return resp, err
 	}
 
-	err = json.Unmarshal(resp, &listResponse)
-	return listResponse, err
+	err = json.Unmarshal(jsonResp, &resp)
+	return resp, err
 }
 
 // Mkdirs creates the given directory and necessary parent directories if they do not exist
-func (a DbfsAPI) Mkdirs(data httpmodels.MkdirsReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/mkdirs", data, nil)
+func (a DbfsAPI) Mkdirs(req httpmodels.MkdirsReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/mkdirs", req, nil)
 	return err
 }
 
 // Move moves a file from one location to another location within DBFS
-func (a DbfsAPI) Move(data httpmodels.MoveReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/move", data, nil)
+func (a DbfsAPI) Move(req httpmodels.MoveReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/move", req, nil)
 	return err
 }
 
 // Put uploads a file through the use of multipart form post
-func (a DbfsAPI) Put(data httpmodels.PutReq) error {
-	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/put", data, nil)
+func (a DbfsAPI) Put(req httpmodels.PutReq) error {
+	_, err := a.Client.performQuery(http.MethodPost, "/dbfs/put", req, nil)
 	return err
 }
 
 // Read returns the contents of a file
-func (a DbfsAPI) Read(data httpmodels.ReadReq) (httpmodels.ReadResp, error) {
-	var readResponse httpmodels.ReadResp
+func (a DbfsAPI) Read(req httpmodels.ReadReq) (httpmodels.ReadResp, error) {
+	var resp httpmodels.ReadResp
 
-	resp, err := a.Client.performQuery(http.MethodGet, "/dbfs/read", data, nil)
+	jsonResp, err := a.Client.performQuery(http.MethodGet, "/dbfs/read", req, nil)
 	if err != nil {
-		return readResponse, err
+		return resp, err
 	}
 
-	err = json.Unmarshal(resp, &readResponse)
-	return readResponse, err
+	err = json.Unmarshal(jsonResp, &resp)
+	return resp, err
 }
